448213080c20258b726ef98408a6cf4d21ecb4e1
HarshithRajesh <harshithr2004@gmail.com>
2023-06-17 21:46:02 +0530

    login completed

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
 from dotenv import load_dotenv
+import time
 load_dotenv()
 
 USERNAME = os.getenv('USERNAME')
@@ -24,13 +25,20 @@ def get_driver():
     options.add_argument("disable-popup-blocking")
 
     driver = webdriver.Chrome(service=service,options=options)
-    driver.get("https://www.linkedin.com/authwall?trk=qf&original_referer=https://www.linkedin.com/feed/&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fhome")
+    driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3420079349&f_AL=true&geoId=105214831&keywords=python%20"
+           "developer&location=Bengaluru%2C%20Karnataka%2C%20India&refresh=true")
     return driver
 
 def main():
     driver = get_driver()
-    driver.find_element(By.ID,"session_key").send_keys(USERNAME)
-    driver.find_element(By.ID,"session_password").send_keys(PASSWORD + Keys.RETURN)
+    sign_in = driver.find_element(By.LINK_TEXT,"Sign in")
+    sign_in.click()
+    time.sleep(1)
+    username = driver.find_element(By.ID,"username")
+    username.send_keys(USERNAME)
+    password = driver.find_element(By.ID,"password")
+    password.send_keys(PASSWORD)
+    password.send_keys(Keys.ENTER)
     return driver.current_url
 
 print(main())
