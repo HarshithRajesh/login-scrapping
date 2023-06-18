a904864958db9056295973c70fb7a8684ff82216
HarshithRajesh <harshithr2004@gmail.com>
2023-06-18 22:58:32 +0530

    login completed of freelancer

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import os
 from dotenv import load_dotenv
 import time
 load_dotenv()
 
-USERNAME = os.getenv('USERNAME')
+USERNAME = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
-
 
 # service = Service('C:\\Development\\chromedriver.exe')
 service = Service(service = Service(ChromeDriverManager().install()))
@@ -25,21 +26,19 @@ def get_driver():
     options.add_argument("disable-popup-blocking")
 
     driver = webdriver.Chrome(service=service,options=options)
-    driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3420079349&f_AL=true&geoId=105214831&keywords=python%20"
-           "developer&location=Bengaluru%2C%20Karnataka%2C%20India&refresh=true")
+    driver.get("https://www.freelancer.com/login")
     return driver
 
 def main():
     driver = get_driver()
-    sign_in = driver.find_element(By.LINK_TEXT,"Sign in")
-    sign_in.click()
-    time.sleep(1)
-    username = driver.find_element(By.ID,"username")
-    username.send_keys(USERNAME)
-    password = driver.find_element(By.ID,"password")
-    password.send_keys(PASSWORD)
-    password.send_keys(Keys.ENTER)
+    time.sleep(2)
+    driver.find_element(By.ID,"emailOrUsernameInput").send_keys(USERNAME)
+    time.sleep(2)
+    driver.find_element(By.ID,"passwordInput").send_keys(PASSWORD+Keys.RETURN)
+    time.sleep(5)
     return driver.current_url
+    
+
 
 print(main())
 
